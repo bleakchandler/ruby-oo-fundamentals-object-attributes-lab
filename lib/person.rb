b948f2f1fd62643ebbc79995763cf3f501b15f7f
Bleak Chandler <bleakchandler@gmail.com>
2021-02-02 19:01:03 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,28 @@
+
+
+
+
+class Person
+   
+
+    def name
+        @name
+    end
+
+
+    def name=(newname)
+        @name = newname
+    end
+
+
+    def job
+        @job
+    end
+
+
+    def job=(newjob)
+        @job = newjob
+    end
+
+
+end
